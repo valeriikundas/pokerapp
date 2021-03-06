6598f11529350dd2658feb12ed2db3129e4fba00
Valerii <valeriykundas@gmail.com>
2021-03-06 22:53:37 +0200

    rename api to apiClient

--- frontend/src/apis/apiClient.ts
+++ frontend/src/apis/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({
+const apiClient = axios.create({
   baseURL: `http://localhost:5000/api`,
 });
 
-export default api;
+export default apiClient;
