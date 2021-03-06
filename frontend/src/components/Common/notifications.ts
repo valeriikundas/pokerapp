2c6a8c7e9cf02276a4d9f19b46c911d76a45ab56
Valerii <valeriykundas@gmail.com>
2021-03-07 00:58:45 +0200

    add notifications

---
+++ frontend/src/components/Common/notifications.ts
@@ -0,0 +1,7 @@
+import { toast } from "react-toastify";
+
+export const showInfoNotification = (message: string) =>
+  toast(message, { type: "info" });
+
+export const showErrorNotification = (message: string) =>
+  toast(message, { type: "error" });
